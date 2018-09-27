308840877a4922922fcaa594ffa10bcb10be5fe6
Janne Hellsten <jjhellst@gmail.com>
2018-09-27 23:17:42 +0300

    make a debugger adapter based on the ms mock debug adapter

---
+++ vscode/client/src/debugAdapter.ts
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+import { C64jasmDebugSession } from './c64jasmDebug';
+
+C64jasmDebugSession.run(C64jasmDebugSession);
