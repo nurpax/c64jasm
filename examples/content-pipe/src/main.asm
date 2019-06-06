cbb34b7d96df10394338e5246daa8829fa2d9171
Janne Hellsten <jjhellst@gmail.com>
2019-06-06 22:47:35 +0300

    imaginary game intro screen example to demonstrate gfx content & sid

--- examples/content-pipe/src/main.asm
+++ examples/content-pipe/src/main.asm
@@ -14,15 +14,15 @@
 ; Load a PETSCII file exported into .json by Petmate.
 ; The petscii loader will RLE compress the screencodes
 ; and colors to save some RAM.
-!let petscii_background = petscii.rlePetsciiJson("assets/petscii.json")
+!let petscii_background = petscii.rlePetsciiJson("assets/pipes-pet.json")
 !let pacman_spd = spd("assets/pacman.spd")
 !let music = sid("assets/Load_Line.sid")
 
-
-!let irq_top_line = 30
+!let irq_top_line = 10
 !let SIN_LEN = 64
+!let SINX_LEN = 256
 
-!let debug_build = TRUE
+!let debug_build = FALSE
 
 !let zptmp0 = $20
 
@@ -30,15 +30,14 @@
 ;--------------------------------------------------------------
 ; Execution starts here
 ;--------------------------------------------------------------
-entry:
-
+entry: {
     lda #0
     jsr music.init
 
     sei
     lda #$35        ; Bank out kernal and basic
     sta $01         ; $e000-$ffff
-    jsr set_raster_irq
+    +setup_irq(irq_top, irq_top_line)
     cli
 
     ; Decompress a PETSCII image on the display
@@ -50,14 +49,30 @@ entry:
     jsr pet_rle::decode
 
 frame_loop:
-    ; wait for vsync (by polling the framecount that's inc'd
-    ; by the raster IRQ)
+    ; wait for vsync by polling the framecount that's inc'd
+    ; by the raster IRQ
     lda framecount
 vsync:
     cmp framecount
     beq vsync
 
-    ; sine animate the sprite
+!if (debug_build) {
+    inc $d020
+}
+    jsr text_color_cycle
+    jsr animate_sprites
+    jsr set_sprite_regs
+
+!if (debug_build) {
+    dec $d020
+}
+    jmp frame_loop
+}
+
+animate_sprites: {
+    !let xanimptr = zptmp0 + 2
+    !let sidx = zptmp0 + 4
+    ; sine animate sprites (y-coord)
     lda #0
     tay
     sta zptmp0
@@ -69,41 +84,83 @@ anim_sprites:
     tax
     lda sintab, x
     clc
-    adc #200
+    adc #142
     sta sprite_ypos, y
     lda zptmp0
-    adc #5
+    clc
+    adc #5          ; no clc needed
     sta zptmp0
     iny
     cpy #8
     bne anim_sprites
 
-!if (debug_build) {
-    inc $d020
-}
-    jsr set_sprites
+    ; animate sprites (x-coord)
+    ; just ping-pong motion which turned out
+    ; to be a lot of code and RAM.. patches are welcome ;)
+    lda #0
+    sta sidx
+    sta zptmp0
+anim_sprites_x:
+    lda framecount
+    clc
+    adc zptmp0
 
-!if (debug_build) {
-    dec $d020
-}
+    ; xanimptr = (framecount+zptmp0)<<1
+    sta xanimptr
+    lda #0
+    sta xanimptr+1
+    lda xanimptr
+    asl
+    sta xanimptr+0
+    rol xanimptr+1
 
-    jmp frame_loop
+    ; xanimptr += xanim_tbl
+    clc
+    adc #<xanim_tbl
+    sta xanimptr+0
+    lda xanimptr+1
+    adc #>xanim_tbl
+    sta xanimptr+1
+
+    ldy #0
+    ldx sidx
+    lda (xanimptr), y
+    sta sprite_xpos+0, x
+    iny
+    lda (xanimptr), y
+    sta sprite_xpos+1, x
+
+    lda zptmp0
+    clc
+    adc #15
+    sta zptmp0
+
+    txa
+    clc
+    adc #2
+    sta sidx
+    cmp #8*2
+    bne anim_sprites_x
+    rts
+}
 
 ; Set sprites to hardware
-set_sprites: {
-    !let xtmp = zptmp0
+set_sprite_regs: {
+    !let xcoord = zptmp0
+    !let x9bit = zptmp0 + 2
+    !let sidx = zptmp0 + 4
 
     lda #0
     sta $d01d       ; no double width
     sta $d017       ; no double height
-    lda #%10101010  ; odd sprites are multicolor, even single
+    lda #%11111111  ; all 8 sprites multicolor
     sta $d01c       ; single color sprites
 
     lda #sprite_data/64
     ldx #sprite_data2/64
     !for i in range(4) {
-        sta $07f8+i*2     ; sprite ptr
-        stx $07f8+i*2+1   ; sprite ptr
+        stx $07f8+i*2     ; sprite ptr
+        sta $07f8+i*2+1   ; sprite ptr
     }
 
     ; Set the pacman multicolor sprite multicolor bits
@@ -112,32 +169,93 @@ set_sprites: {
     lda #pacman_spd.multicol2
     sta $d026
 
-    lda #35
-    sta xtmp
-    ldx #0
-    ldy #0
+    lda #0
+    sta sidx
+    sta x9bit
 xloop:
-    lda xtmp        ; x coord
+    lda sidx
+    asl
+    tax
+    lda sprite_xpos + 0, x
+    sta xcoord
+    lda sprite_xpos + 1, x
+    sta xcoord+1
+
+    lda xcoord        ; x coord
     sta $d000, x
-    clc
-    adc #30
-    sta xtmp
+
+    ldy sidx
+    lda sprite_ypos, y
+    sta $d001, x
 
     lda sprite_color, y
     sta $d027, y
 
-    lda sprite_ypos, y
-    sta $d001, x
-    iny
-    inx
-    inx
-    cpx #16
+    ; work out 9th bit of sprite x-coord
+    lda xcoord+1
+    and #1
+    beq lt256
+    ldx sidx
+    lda x9bit
+    ora shift_lut, x
+    sta x9bit
+lt256:
+
+    inc sidx
+    cpy #7
     bne xloop
 
     ; Enable all sprites
     lda #%11111111
     sta $d015
+    lda x9bit
+    sta $d010
     rts
+
+shift_lut:
+!for i in range(8) {
+    !byte 1<<i
+}
+}
+
+text_color_cycle: {
+    !let zx1 = zptmp0
+    !let zx2 = zptmp0+1
+
+    lda framecount
+    lsr
+    lsr
+    sta zx1
+
+    ; rotate the bottom row in reverse direction
+    lda #0
+    sec
+    sbc zx1
+    sta zx2
+
+    ldx #0
+cycle:
+    lda zx1
+    and #7
+    tay
+    lda colors1, y
+    sta $d800+40, x
+
+    lda zx2
+    and #7
+    tay
+    lda colors2, y
+    sta $d800+23*40, x
+
+    inc zx1
+    inc zx2
+    inx
+    cpx #40
+    bne cycle
+
+    rts
+colors1: !byte 11, 12, 15, 1, 1, 15, 12, 11
+colors2: !byte 6, 14, 3, 1, 1, 3, 14, 6
 }
 
 irq_top: {
@@ -150,14 +268,10 @@ irq_top: {
 end:
 }
 
-set_raster_irq: {
-    +setup_irq(irq_top, irq_top_line)
-    rts
-}
-
 framecount:     !byte 0
-sprite_ypos:    !fill 8, 0
-sprite_color:   !byte 1, 7, 8, 9, 10, 2, 13, 14
+sprite_xpos:    !fill 8*2, 0    ; word
+sprite_ypos:    !fill 8, 0      ; byte
+sprite_color:   !byte 3, 10, 3, 9, 3, 2, 3, 14
 
 !let sinvals = math.sintab(SIN_LEN, 30)
 sintab:
@@ -165,35 +279,18 @@ sintab:
     !byte v
 }
 
-!align 64
-sprite_data:
-!for y in range(21) {
-    !for x in range(3) {
-        !let bits = 0
-        !for xi in range(8) {
-            !let xx = x*8 + xi
-            !let ox = xx - 24/2
-            !let oy = y - 21/2
-            !let r = ox*ox + oy*oy
-            !let v = 0
-            !if (r < 10*10) {
-                v = 1
-            }
-            !if (r < 5*5) {
-                v = 0
-            }
-            bits = bits | (v << (7-xi))
-        }
-        !byte bits
-    }
-}
-!byte 0  ; pad to 64 bytes
+xanim_tbl:
+!for v in range(128) { !word (v*2-128) + 344/2 }
+!for v in range(128) { !word 344/2 - (v*2-128) }
 
-sprite_data2:
-!byte pacman_spd.data[0]  ; shape: data[num_sprites][64]
-
-* = music.startAddress
+* = music.startAddress  ; most sids will go to $1000
 sid_data: !byte music.data
+
+* = $2000
+sprite_data:
+!byte pacman_spd.data[0]   ; shape: data[num_sprites][64]
+sprite_data2:
+!byte pacman_spd.data[1]   ; shape: data[num_sprites][64]
 
 !include "pet_rle.asm"
 ; Expand the RLE compressed PETSCII bytes
