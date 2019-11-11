71136f1eaeca3a66c4852ea237c98b16b050040c
Janne Hellsten <jjhellst@gmail.com>
2019-11-11 23:06:47 +0200

    add crlf syntax test
    Add gitattributes -- this is an attempt to force git to use
    binary format for files ending in _crlf.input.asm.

---
+++ test/cases/charset1_crlf.input.asm
@@ -0,0 +1,11 @@
+; This file uses CRLF -- should be set to binary in
+; git attributes so that no CRLF conversion happens for
+; this file.
+
+!let foo = {
+  a: 34
+}
+
+    lda #1
+    ldx #2
+    rts
