f70ca97cca4354594f795b0acb99014d4853c836
shazz <shazz.ng@gmail.com>
2021-02-10 19:40:49 -0500

    Add illegal opcode support (#79)
    + tests
    
    tests cross compared against ca65

---
+++ test/cases/illegal.input.asm
@@ -0,0 +1,119 @@
+; disasm: cycles
+
+;    .org $0801
+
+    slo $20
+    slo $20,x
+    slo $2000
+    slo $2000,x
+    slo $2000,y
+    slo ($44,x)
+    slo ($44),y
+
+    nop
+
+    anc #0
+
+    nop
+
+    rla $20
+    rla $20,x
+    rla $2000
+    rla $2000,x
+    rla $2000,y
+    rla ($44,x)
+    rla ($44),y
+
+    nop
+
+    sre $20
+    sre $20,x
+    sre $2000
+    sre $2000,x
+    sre $2000,y
+    sre ($44,x)
+    sre ($44),y
+
+    nop
+
+    rra $20
+    rra $20,x
+    rra $2000
+    rra $2000,x
+    rra $2000,y
+    rra ($44,x)
+    rra ($44),y
+
+    nop
+
+    sax $20
+    sax $20,y
+    sax $2000
+    sax ($44,x)
+
+    nop
+
+    ; ahx $2000,y
+    ; ahx ($44),y
+
+    nop
+
+    ; xaa #0
+
+    nop
+
+    tas $2000,y
+
+    nop
+
+    shy $2000,x
+
+    nop
+
+    shx $2000,y
+
+    nop
+
+    lax $0
+    lax $20
+    lax $20,y
+    lax $2000
+    lax $2000,y
+    lax ($44,x)
+    lax ($44),y
+
+    nop
+
+    las $2000,y
+
+    nop
+
+    dcp $20
+    dcp $20,x
+    dcp $2000
+    dcp $2000,x
+    dcp $2000,y
+    dcp ($44,x)
+    dcp ($44),y
+
+    nop
+
+    isc $20
+    isc $20,x
+    isc $2000
+    isc $2000,x
+    isc $2000,y
+    isc ($44,x)
+    isc ($44),y
+
+    nop
+
+    alr #0
+
+    nop
+
+    axs #0
+
+    nop
+
+    arr #0
